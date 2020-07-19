bff187c5487790a8ad48e82c406d2823d660552f
rafafrag <31895680+rafafrag@users.noreply.github.com>
2020-07-19 15:58:23 +0100

    styled components and googlefonts added

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
+import WelcomeMessage from "./WelcomeMessage";
+import styled from "styled-components";
+
+const Button = styled.div`
+  background-color: #a52a2a;
+  color: #ffffff;
+`;
 
 class App extends Component {
   render() {
-    return <div> hello </div>;
+    return (
+      <div>
+        <WelcomeMessage />
+        <Button> Button </Button>
+      </div>
+    );
   }
 }
 
