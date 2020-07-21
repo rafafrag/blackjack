d9976a869b5d0a68fec67b1f32edd1dc6dc649f1
rafafrag <31895680+rafafrag@users.noreply.github.com>
2020-07-21 18:51:03 +0100

    refactor to arrow fucntions

--- src/App/Hand.js
+++ src/App/Hand.js
@@ -5,9 +5,9 @@ const Hand = (props) => (
   <div className="hand">
     {props.showDeck ? <Card hidden={true} /> : ""}
 
-    {props.hand.map(function (card, i) {
-      return <Card face={card.f} value={card.v} key={i} />;
-    })}
+    {props.hand.map((card, i) => (
+      <Card face={card.f} value={card.v} key={i} />
+    ))}
   </div>
 );
 Hand.defaultProps = { hand: [] };
